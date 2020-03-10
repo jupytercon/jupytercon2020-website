9b4874dd26bf6eff2886e062c2e2158d7f83bf1d
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-03-10 15:54:17 +0100

    make minmax container, and add a common stylesheet

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,3 +1,13 @@
 <template>
-  <nuxt/>
+  <div class="main">
+    <nuxt/>
+  </div>
 </template>
+<style scoped>
+  @import 'assets/scss/common.scss';
+  .main {
+    margin: 0 auto;
+    min-width: 1024px;
+    max-width: 1440px;
+  }
+</style>
