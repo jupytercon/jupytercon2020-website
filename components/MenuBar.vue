894417790ee332796e4fdf9f0fee03420ac82fee
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-03-12 17:13:53 +0100

    fill in privacy policy, solved #43

--- components/MenuBar.vue
+++ components/MenuBar.vue
@@ -19,7 +19,7 @@
                         <a href="https://twitter.com/JupyterCon"><div class="social-icon twitter-icon"></div></a>
                         <a href="https://www.youtube.com/user/PyDataTV"><div class="social-icon youtube-icon"></div></a>
                     </div>
-                    <nuxt-link to="/privacy/"><div class="terms">Privacy Policy | Terms & Conditions</div></nuxt-link>
+                    <nuxt-link to="/privacy/"><div class="terms">Privacy Policy</div></nuxt-link>
                     <div class="menu-columns">
                         <MenuContent></MenuContent>
                     </div>
