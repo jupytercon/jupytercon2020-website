e1290d33ca7be446aab71a57c87fd4d23ba532ef
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-03-12 15:22:49 +0100

    fix: favicon respecting routerBase

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ export default {
       { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: 'icon', type: 'image/x-icon', href: (routerBase.base || '/') + 'favicon.ico' },
       { rel: "stylesheet", href: "https://fonts.googleapis.com/css?family=IBM+Plex+Mono:500|IBM+Plex+Sans:400,500,600,700&display=swap" },
     ]
   },
