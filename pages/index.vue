d68699c3f0e91763b175a986abfa878b475e0e79
Maarten A. Breddels <maartenbreddels@gmail.com>
2020-03-12 10:16:52 +0100

    fix: replace lorem by real text, fixes #32 #26

--- pages/index.vue
+++ pages/index.vue
@@ -3,14 +3,18 @@
         <BlockHeader title="JupyterCon" subtitle="2020">
         </BlockHeader>
 
-        <BlockAbout :img="require('~/assets/images/flickr/people_smiling_sitting.jpg')" title="About"/>
+        <BlockAbout :img="require('~/assets/images/flickr/people_smiling_sitting.jpg')" title="About">
+            <p>JupyterCon brings together data scientists, business analysts,
+            researchers, educators, developers, and core Project contributors
+            and tool creators for in-depth training, insightful keynotes,
+            networking events, and practical talks exploring the Project Jupyter
+            platform.
+            </p>
+        </BlockAbout>
         <BlockGreyOrange>
             <div class="main-text main-content float-left">
                 <h2>Participate</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Turpis risus fames dolor volutpat risus vitae.
-                    Malesuada elit nec nibh elementum at faucibus nisl. Scelerisque elit, vel cursus scelerisque. Sed ut
-                    malesuada pretium faucibus odio quis
-                    congue.</p>
+                <p>Do you have a great idea or tool for the Jupyter community you would like to share? Do you have a really interesting application where you've used Jupyter heavily and have developed good workflows for using Jupyter software that are broadly applicable? What lessons do you have for others using or deploying Jupyter?</p>
                 <nuxt-link class="button hollow" to="/participate/">Learn More</nuxt-link>
             </div>
             <img src="~assets/images/speed-network.jpg" class="float-left"></img>
