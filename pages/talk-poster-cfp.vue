59e8979fe21fefc1577a6a76cec6cdd7cc572235
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-03-13 11:31:38 +0100

    Add tutorials and talks cfps

---
+++ pages/talk-poster-cfp.vue
@@ -0,0 +1,206 @@
+<template>
+    <div>
+        <BlockHeaderPlain title="Propose a talk or poster">
+            <template  v-slot:side-content>
+            </template>
+        </BlockHeaderPlain>
+
+        <div class="main grey-background">
+
+            <p>
+                Do you have a great idea or tool for the Jupyter community you would like to share? Do you have a really interesting application where you've used Jupyter heavily and have developed good workflows for using Jupyter software that are broadly applicable? What lessons do you have for others using or deploying Jupyter? What do you wish you would have known when starting to use Jupyter or interacting in the Jupyter community? What has been the slowest or most frustrating thing you've had to learn about using Jupyter that you could share with others to make their experience better? Propose a talk or poster at the JupyterCon 2020!
+            </p>
+
+            <h2>Talks at JupyterCon 2020</h2>
+
+                <p>
+                    JupyterCon talks are 30 minutes long (plan for 25 minutes of presentation and 5 minutes of questions). Talks are organized into the following tracks:
+                </p>
+
+                <h3>Enterprise Jupyter Infrastructure</h3>
+
+                    <p>
+                        Talks that address deploying Jupyter and JupyterHub at scale in industry, government, high-performance computing, science, education, and other settings. Topics include DevOps, scaling services, security concerns, regulation compliance, and more.
+                    </p>
+
+                <h3>Jupyter in Education</h3>
+
+                    <p>
+                        Talks that address how to teach and learn using Jupyter in a variety of settings.
+                    </p>
+
+                <h3>Jupyter in scientific research</h3>
+
+                    <p>
+                        Talks that address how Jupyter is used in specific scientific research fields, such as climate change, physics, astronomy, meteorology, and more.
+                    </p>
+
+                <h3>Data science applications</h3>
+
+                    <p>
+                        Talks that address how Jupyter is used by data science practitioners in industry, government, and elsewhere.
+                    </p>
+
+                <h3>Jupyter community: tools and practices</h3>
+
+                    <p>Talks that address the community tooling and best practices in the Jupyter ecosystem. Community tools include frontends, kernels, extensions, and other tools in the Jupyter ecosystem. Community practices include how Jupyter is used in a workflow and how the Jupyter community itself works.</p>
+
+            <h2>Posters at JupyterCon 2020</h2>
+
+                <p>
+                    If your work is in progress, this is the place to share and get feedback. More suited for topics relevant to a narrow subset of the Jupyter community, poster presentations encourage one-on-one discussions between presenters and attendees that continue far beyond conference halls. 
+                </p>
+
+                <p>
+                    Posters is a forum for ideas to be presented in a concise, visually appealing way. Anyone is welcome to submit to the Posters program: researchers, practitioners, enthusiasts, and students. The strongest posters focus on clear communication and consideration of all audiences. Posters should be A0 size or at most 36 inches by 48 inches.
+                </p>
+
+            <h2>Dates to remember</h2>
+
+                <table style="width:100%">
+                  <tr>
+                    <td>March 16th, 2020</td>
+                    <td>Call for proposals opens</td>
+                  </tr>
+                  <tr>
+                    <td>April 13th, 2020</td>
+                    <td>Call for proposals closes</td>
+                  </tr>
+                  <tr>
+                    <td>April 14th - May 3rd, 2020</td>
+                    <td>Review period</td>
+                  </tr>
+
+                  <tr>
+                    <td>May 4th, 2020</td>
+                    <td>Acceptance/rejection notifications are sent</td>
+                  </tr>
+                  <tr>
+                    <td>May 25th, 2020</td>
+                    <td>Schedule announcement</td>
+                  </tr>
+                  <tr>
+                    <td>August 12-13th, 2020</td>
+                    <td>Talks and posters are presented at JupyterCon 2020</td>
+                  </tr>
+                </table>
+
+            <h2>Guidelines for submitting a talk or poster</h2>
+
+                <h3>Speaker Profile</h3>
+
+                    <p>
+                        To submit a talk, you will first need to create a speaker profile. This includes information such as your name (as you would like it to appear in the conference program), and the following optional information, which will be published as a part of the conference program if your talk is accepted:
+                    </p>
+
+                    <ul>
+                        <li>a short biography telling the audience a little about you</li>
+                        <li>a photo</li>
+                        <li>a GitHub and/or Twitter handles to connect with you</li>
+                    </ul>
+
+                    <p>The primary presenter should submit the proposal, and afterwards the proposal can be edited to add other authors.</p>
+
+                <h3>Proposal</h3>
+
+                    <p>
+                        A talk or poster proposal must include the following information that will be listed publicly in the conference program for accepted talks or posters.
+                    </p>
+
+                    <ol>
+                        <li>
+                            <strong>Title:</strong> Give your talk a title that accurately describes its content and focus to the potential attendees.
+                        </li>
+                        <li>
+                            <strong>Audience:</strong> Describe what level of programming/coding and other topic-specific knowledge, experience or expertise your talk is aiming for. Will the attendees need multiple skills or proficiency in tools other than Jupyter notebooks?<br>
+                            ‘Advanced’, ‘Intermediate’, and ‘Beginner’ may mean different things to different people. Feel free to include any additional details regarding prerequisites for your talk, as well as what audience may benefit from it. 
+Brief summary: Submit a short paragraph describing the topic and key takeaways for attendees. The description will be included in the conference program announcements, should it be selected.
+                        </li>
+                        <li>
+                            <strong>A detailed outline:</strong> This is a self-contained statement that summarises the objective of the talk or poster, its outline, central thesis, and key takeaways. After reading the description, the audience should have an idea of the overall presentation and know what to expect. The description should also make clear what background knowledge is expected from the attendees. Include links to relevant source code, articles, videos, or other information that add context to your proposal.
+                        </li>
+                        <li>
+                            <strong>Your affiliation:</strong> for the purposes of this talk or poster.
+                        </li>
+                    </ol>
+
+            <h2>Talk and poster proposal submission FAQs</h2>
+
+                <ol>
+                    <li>
+                        <p class="question">
+                            What can I do to increase the likelihood of my talk/poster being accepted?
+                        </p>
+                        <p>
+                            Here are some common pitfalls that could lead to the proposal being rejected:
+                        </p>
+                        <ul>
+                            <li>Submit early. Submissions can be modified up until the submission deadline.</li>
+                            <li>Ensure your talk or poster will appeal to a wider swatch of the Jupyter community. If your talk topic addresses a narrow segment of issues, users, or applications, consider shifting focus or highlighting best practices and themes that apply to a more general audience. People from many fields and backgrounds attend JupyterCon, which can lead to rich discussions among attendees that would not normally have a chance to interact with each other. What lessons have you learned that can benefit others in the Jupyter community that may not be from your specific field or application?</li>
+                            <li>Take a look at the <a href="https://conferences.oreilly.com/jupyter/jup-ny-2017/public/schedule/grid/public">JupyterCon 2017</a> and <a href="https://conferences.oreilly.com/jupyter/jup-ny/public/schedule/full/public">JupyterCon 2018</a> conference schedules for previously accepted talks.</li>
+                            <li>Clearly identify who your target audience is, what background is required for attendees to benefit from your presentation, and what the key takeaways are.</li>
+                            <li>Get feedback, especially from those in your target audience.</li>
+                            <li>There is a limited amount of financial aid to attend the conference. The financial aid application deadlines coincide with the call for proposals deadlines, so if you apply for financial aid to attend, you should know the results before you will need to confirm acceptance.</li>
+                        </ul>
+                    </li>
+                    <li>
+                        <p class="question">
+                            What should I avoid in my talk proposal?
+                        </p>
+
+                        <p>
+                            Here are some common pitfalls that could lead to the proposal being rejected:
+                        </p>
+
+                        <ul>
+                            <li><strong>Overly long proposal</strong> Keep it simple, clear and focused. Good proposals provide the most important information within the brief summary.</li>
+                            <li><strong>Future work</strong> While talking about future work is interesting and could be mentioned in your talk, the core content of the talk should already be shaped, and you should be able to describe it in your proposal.</li>
+                            <li><strong>Sales pitches</strong> We are a community of open source developers and users of open source scientific computing tools. You can reference your closed-source product or platform, but the audience will find the talk more interesting if they can try your techniques within the Jupyter ecosystem. Your problem definition, proposed techniques, broad issues you solved, and business domain might be interesting, but sales pitches and "how to use our product" talks are typically rejected.</li>
+                            <li><strong>Repeated Talk</strong> We have a strong preference for new material and fresh faces, and as such, if your talk is already available online it is unlikely to be accepted for the conference.</li>
+                        </ul>
+                    </li>
+                </ol>
+
+                <p>
+                    The tips above were adapted from the PyData resources. We also recommend checking out blog posts by <a href="http://www.craigkerstiens.com/2012/06/19/pro-tips-for-conference-talks/">Craig Kerstiens</a> and <a href="https://hynek.me/articles/speaking/">Hynek Schlawack</a> for more great advice on this topic.
+                </p>
+
+            <h2>Review Process</h2>
+
+                <p>
+                    Talks and posters will each be reviewed by several community reviewers in open reviews (i.e. identities of the submitters and reviewers are known to each other). The proposals and reviews will be submitted to the co-chairs of each track, who then will make a decision on which talks and posters should be accepted in the JupyterCon 2020 program. Lastly, the JupyterCon 2020 Organizing Committee will review the selected proposals for the final approval. All the submitters will be notified of the outcome. Those whose talks and posters have been accepted will be invited to confirm their participation. The Program Committee will prepare a schedule of the accepted talks.
+                </p>
+
+                <p>
+                    Reviewers play an essential role in creating great JupyterCon programming. Sign up to be a reviewer <a href="https://docs.google.com/forms/d/e/1FAIpQLSdzEoFUh6zb8liiWeP45PjyUtc0rlE-GPSR-UyNRnfR6WqayA/viewform">here</a>.
+                </p>
+
+        </div>
+        <DesktopFooter />
+    </div>
+</template>
+
+<script>
+    import BlockHeaderPlain from "../components/BlockHeaderPlain";
+    import DesktopFooter from "../components/DesktopFooter";
+
+    export default {
+        name: "proposals",
+        components: {DesktopFooter, BlockHeaderPlain},
+    }
+</script>
+
+<style scoped lang="scss">
+    @import 'assets/scss/settings';
+    @import 'assets/scss/common';
+    .menubar {
+        position: relative;
+        z-index: 1;
+    }
+    .main {
+        padding: 100px;
+    }
+    .question {
+        font-weight: bold;
+    }
+</style>
