ae92f95ac317a712531998f9bfd25cf93fda4b89
Sylvain Corlay <sylvain.corlay@gmail.com>
2020-10-12 18:22:17 +0200

    Add Tema keynote announcement

--- pages/blog.vue
+++ pages/blog.vue
@@ -37,6 +37,7 @@
         data() {
             return {
                urls: [
+                   "https://blog.jupyter.org/tema-okun-a7e5f1f0cd05",
                    "https://blog.jupyter.org/benjamin-ragan-kelley-59a6bf015ea9",
                    "https://blog.jupyter.org/omoju-miller-435db40d347e",
                    "https://blog.jupyter.org/lucy-dagostino-mcgowan-13adbdedccf0",
